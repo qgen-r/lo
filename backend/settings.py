191f052853aa19d90c1781906ad64367349377a0
nailaAbd <cyrynbd@gmail.com>
2025-06-24 09:19:50 +0300

    fix: static root error

--- backend/settings.py
+++ backend/settings.py
@@ -132,7 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
