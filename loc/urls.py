2ed90efd2dfaecaaf58d3c098ae1e5babe8fe165
nailaAbd <cyrynbd@gmail.com>
2025-06-24 11:40:17 +0300

    fix: homepage route

--- loc/urls.py
+++ loc/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("verify/", views.verify_page, name="verify"),
+    path("", views.verify_page, name="verify"),
     path("save-ip/", views.save_ip, name="save-ip"),
     path("save-gps/", views.save_gps, name="save-gps"),
     path("send-otp/", views.send_otp, name="send-otp"),
